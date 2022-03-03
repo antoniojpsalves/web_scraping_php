bab96d3b060de13d07eb1ccd27ed3a3c3444583f
Antonio <antonio.jpsalves@gmail.com>
2022-03-03 18:12:55 -0300

    Comentando o código

--- buscador.php
+++ buscador.php
@@ -1,18 +1,35 @@
 <?php
 
+//Ocultando os warnings.
 libxml_use_internal_errors(true);
 
+//Variável usada para debug
 $debug = isset($_GET['debug']) ? true : false;
+
+/**
+ * Primeiro passo a fazer é realizar uma requisição http para a página, e obtendo o body como resposta
+ */
 $conteudo = file_get_contents('https://dias.dev/');
 
-$documento = new DOMDocument();
 
+/**
+ * Utilizando o dom para manipular o objeto recebido
+ */
+$documento = new DOMDocument();
 $documento->loadHTML($conteudo);
 
 
+/**
+ * Percorrendo elementos xml ou html usando 'xpath'
+ * Isso retorna uma dom node list com os elementos
+ */
 $xPath = new DOMXPath($documento);
 $domNodeList = $xPath->query('.//h2[@itemprop="headline"]');
 
+
+/**
+ * Percorrendo o elemento e obtendo o texto.
+ */
 foreach($domNodeList as $element) {
     echo $element->textContent . "<br>";
 }
